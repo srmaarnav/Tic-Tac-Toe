562555b4047a9dd266bba2a03c8b9eb084988f0f
srmaarnav <arnav6266@gmail.com>
2023-10-26 10:09:22 +0545

    Screen

--- game.py
+++ game.py
@@ -15,12 +15,24 @@ screen.fill(BG_COLOR)
 
 class Game:
     def __init__(self):
-        pass
+        self.show_lines()
+
+    def show_lines(self):
+        # vertical
+        pygame.draw.line(screen, LINE_COLOR, (SQSIZE, 0), (SQSIZE, HEIGHT), LINE_WIDTH)
+        pygame.draw.line(screen, LINE_COLOR, (WIDTH - SQSIZE, 0), (WIDTH - SQSIZE, HEIGHT), LINE_WIDTH)
+        
+        #horizontal
+        pygame.draw.line(screen, LINE_COLOR, (0, SQSIZE), (WIDTH, SQSIZE), LINE_WIDTH)
+        pygame.draw.line(screen, LINE_COLOR, (0, HEIGHT - SQSIZE), (WIDTH, HEIGHT - SQSIZE), LINE_WIDTH)
 
     
 
 # MAIN
 def main():
+    # object
+    game = Game()
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
