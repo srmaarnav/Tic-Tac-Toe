07c0bfb577af518870fbba9248caf19a4735b843
srmaarnav <arnav6266@gmail.com>
2023-10-27 16:59:45 +0545

    27-oct

--- game.py
+++ game.py
@@ -2,7 +2,7 @@
 
 import sys
 import pygame
-import copy
+import numpy as np
 
 from constants import *
 
@@ -13,8 +13,22 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('TIC-TAC-TOE')
 screen.fill(BG_COLOR)
 
+class Board:
+    def __init__(self) -> None:
+        self.squares = np.zeros((ROWS, COLS))
+        
+        
+    def mark_square(self, row, col, player):
+        self.squares[row][col] = player
+    
+    def empty_sqr(self, row, col):
+        return self.squares[row][col] == 0
+    
+    
 class Game:
     def __init__(self):
+        self.board = Board()
+        self.player = 1 # 1 - crosses 2 - circle
         self.show_lines()
 
     def show_lines(self):
@@ -25,19 +39,53 @@ class Game:
         #horizontal
         pygame.draw.line(screen, LINE_COLOR, (0, SQSIZE), (WIDTH, SQSIZE), LINE_WIDTH)
         pygame.draw.line(screen, LINE_COLOR, (0, HEIGHT - SQSIZE), (WIDTH, HEIGHT - SQSIZE), LINE_WIDTH)
-
+        
+    def draw_fig(self, row, col):
+        if self.player == 1:
+            # draw cross
+            # desc line
+            start_desc = (col * SQSIZE + OFFSET, row * SQSIZE + OFFSET)
+            end_desc = (col * SQSIZE + SQSIZE - OFFSET, row * SQSIZE + SQSIZE - OFFSET)
+            pygame.draw.line(screen, CROSS_COLOR, start_desc, end_desc, CROSS_WIDTH)
+            
+            # asc line
+            start_asc = (col * SQSIZE + OFFSET, row * SQSIZE + SQSIZE - OFFSET)
+            end_asc = (col * SQSIZE + SQSIZE - OFFSET, row * SQSIZE + OFFSET)
+            pygame.draw.line(screen, CROSS_COLOR, start_asc, end_asc, CROSS_WIDTH)
+        
+        elif self.player == 2:
+            # draw circle
+            center = (col * SQSIZE + SQSIZE // 2, row * SQSIZE + SQSIZE // 2)
+            pygame.draw.circle(screen, CIRC_COLOR, center, RADIUS, CIRC_WIDTH)
     
+    def next_turn(self):
+        self.player = self.player % 2 + 1
+
 
 # MAIN
 def main():
     # object
     game = Game()
+    board = game.board
     
+    
+    # main loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+                
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = event.pos
+                row = pos[1] // SQSIZE
+                col = pos[0] // SQSIZE
+                
+                if board.empty_sqr(row, col):
+                    board.mark_square(row, col, game.player)
+                    game.draw_fig(row, col)
+                    game.next_turn()
+                print(board.squares)
         
         pygame.display.update()
 
